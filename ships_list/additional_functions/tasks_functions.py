dea652bf18917dac8484d50f01a92c3484d8b5c0
Vitaliy <vitatomchyk@gmail.com>
2022-07-18 06:43:55 +0200

    Added function remove_task

--- ships_list/additional_functions/tasks_functions.py
+++ ships_list/additional_functions/tasks_functions.py
@@ -1,6 +1,6 @@
 from ships_list.additional_functions.additional_functions \
     import append_JSON_file, id_generator, missing_arguments_checker,\
-    json_read, json_write, dictionary_finder
+    read_JSON_file, write_JSON_file, dictionary_finder
 
 
 def add_task(ship, task, stage, party):
@@ -24,16 +24,16 @@ def add_task(ship, task, stage, party):
         append_JSON_file(the_task, file)
         print('Task has been added.')
 
-        ships_list = json_read("ships_list/lists/ships.json")
+        ships_list = read_JSON_file("ships_list/lists/ships.json")
         the_ship = dictionary_finder(ships_list, ship, "ships_name")
         the_ship["has_tasks"] = True
         the_ship["number_of_tasks"] += 1
-        json_write("ships_list/lists/ships.json", ships_list)
+        write_JSON_file("ships_list/lists/ships.json", ships_list)
 
 
 def read_tasks_list(ship):
 
-    tasks_list = json_read("ships_list/lists/tasks.json")
+    tasks_list = read_JSON_file("ships_list/lists/tasks.json")
 
     print(f'This are the tasks you have for ship {ship}:')
 
@@ -41,13 +41,13 @@ def read_tasks_list(ship):
         if task['ships_name'] == ship:
             print(f'-{task["task_title"]}')
 
-    json_write("ships_list/lists/tasks.json", tasks_list)
+    write_JSON_file("ships_list/lists/tasks.json", tasks_list)
 
 
 def remove_task(id):
 
-    tasks_list = json_read("ships_list/lists/tasks.json")
-    ships_list = json_read("ships_list/lists/ships.json")
+    tasks_list = read_JSON_file("ships_list/lists/tasks.json")
+    ships_list = read_JSON_file("ships_list/lists/ships.json")
     i = 0
     while i < len(tasks_list):
         i += 1
@@ -58,5 +58,5 @@ def remove_task(id):
             break
     del tasks_list[1]
 
-    json_write("ships_list/lists/tasks.json", tasks_list)
-    json_write("ships_list/lists/ships.json", ships_list)
+    write_JSON_file("ships_list/lists/tasks.json", tasks_list)
+    write_JSON_file("ships_list/lists/ships.json", ships_list)
