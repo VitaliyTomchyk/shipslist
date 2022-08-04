d5e9a4de690c105a444996a7755498ab5143be85
Dmitriy Chebruchan <chebruchan2707@gmail.com>
2022-08-04 23:22:39 +0300

    refactoring of add_voyage function

--- ships_list/additional_functions/voyage_functions.py
+++ ships_list/additional_functions/voyage_functions.py
@@ -7,12 +7,30 @@ from ships_list.additional_functions.json_functions import write_JSON_file, \
     append_JSON_file, read_JSON_file
 
 
+def appender_of_stages(voyage, result):
+
+    for load_port in voyage['l_ports']:
+        result.append(['Prior arrival at load port {}'.format(load_port)])
+        result.append(['At load port {}'.format(load_port)])
+
+    for discharge_port in voyage['d_ports']:
+        result.append(['Prior arrival at discharge ' +
+                       'port {}'.format(discharge_port)])
+        result.append(['At discharge port {}'.format(discharge_port)])
+
+    for restr_point in voyage['restr_point']:
+        result.append(['Prior arrival at load port {}'.format(restr_point)])
+        result.append(['At load port {}'.format(restr_point)])
+
+    return result
+
+
 def voyage_stages_generator(voyage):
 
     standard_stages = read_JSON_file(SUPPORTING_FILE)['stages']
-    # result = ['Prior delivery at ' + voyage['delivery_point'],
-    #           'After delivery at ' + voyage['delivery_point']]
-    result = standard_stages
+    result = [standard_stages[0], standard_stages[1]]
+    result = appender_of_stages(voyage, result)
+    result.append('After redelivery')
 
     return result
 
@@ -22,11 +40,13 @@ def input_point(type, quantity_of_ports=None):
     result = []
     if quantity_of_ports is None:
         quantity = int(input('Please put quantity of {}\n'.format(type)))
+    else:
+        quantity = quantity_of_ports
 
     i = 0
     while i < quantity:
-        text = 'Please put name of point number {}\n'.format(i + 1)
-        result.append[input(text)]
+        text = 'Please put name of {} number {}\n'.format(type, i + 1)
+        result.append(str(input(text)))
         i = i + 1
 
     return result
@@ -37,11 +57,11 @@ def add_voyage():
     result = {"id": id_generator(),
               "charterer": input('\nPut name of Charterers company\n'),
               "cargo": input("\nPut name of cargo\n"),
-              "weight_of_cargo": input("\nPut eight of cargo in tonns\n"),
+              "weight_of_cargo": input("\nPut weight of cargo in tonns\n"),
               "ship": input_option(SHIPS_FILE, 'ships_name',
                                    'ship\'s name'),
-              "delivery_point": input_point('delivery point', 1),
 
+              "delivery_point": input_point('delivery point', 1),
               "del_point": input_point('delivery point', 1),
               "l_ports": input_point('load port(s)'),
               "d_ports": input_point('disch port(s)'),
