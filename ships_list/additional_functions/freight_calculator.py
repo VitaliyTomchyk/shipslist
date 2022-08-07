8e8e4efbe97f5e6179d856c68d23c04ecec08f64
Dmitriy Chebruchan <chebruchan2707@gmail.com>
2022-08-07 14:52:23 +0300

    freight calculator was added

--- ships_list/additional_functions/freight_calculator.py
+++ ships_list/additional_functions/freight_calculator.py
@@ -4,18 +4,19 @@ def freight_calculator():
         "freight_rate": int(input('Input freight rate, USD per mt\n')),
         "commition_on_freight": int(input('Commition on freight, %\n')) / 100,
         "cargo_quantity": int(input('Quantity of cargo, mt\n')),
+
         "hire_rate": int(input('Hire rate, USD per day\n')),
         "commition_on_hire": int(input('Commition on hire, %\n')) / 100,
         "duration": int(input('Duration of voyage, days\n'))
     }
 
-    total_freight = input_information['freight_rate'] * \
-        input_information['cargo_quantity'] * \
-        (1 - input_information['commition_on_freight'])
+    total_freight = total_calculator(input_information['freight_rate'],
+                                     input_information['cargo_quantity'],
+                                     input_information['commition_on_freight'])
 
-    total_hire = input_information['hire_rate'] * \
-        input_information['duration'] * \
-        (1 - input_information['commition_on_hire'])
+    total_hire = total_calculator(input_information['hire_rate'],
+                                  input_information['duration'],
+                                  input_information['commition_on_hire'])
 
     profit = round(total_freight - total_hire, 2)
     daily_profit = round(profit / input_information['duration'], 2)
@@ -24,3 +25,7 @@ def freight_calculator():
     print('Daily profit is expected to be USD {}.\n'.format(daily_profit))
 
     return profit
+
+
+def total_calculator(rate, base, persentage):
+    return round(rate * base * (1 - persentage), 2)
