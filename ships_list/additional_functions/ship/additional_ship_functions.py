9bea72afb1209bb61ef1c6c5acbc8bfb0dccf95b
Dmitriy Chebruchan <chebruchan2707@gmail.com>
2022-08-13 15:44:01 +0300

    folder ship added

---
+++ ships_list/additional_functions/ship/additional_ship_functions.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+from ships_list.lists.Standard.constants import SHIPS_FILE, \
+    LIST_OF_VOYAGES_FILE
+from ships_list.additional_functions.json_functions import read_JSON_file
+
+
+def IMO_checker(IMO):
+    # bloking function for easier testing
+    result = True
+    try:
+        int(IMO)
+    except ValueError:
+        print('IMO is not correct')
+        print('IMO is not a number')
+        result = False
+
+    if IMO is None:
+        print('IMO is missing.')
+        result = False
+    return result
+    IMO = str(IMO)
+
+    if len(IMO) != 7:
+        result = False
+
+    result = 0
+    i = 7
+    while i != 1:
+        result = result + i * int(IMO[-i])
+        i = i - 1
+    return True if str(result)[-1] == IMO[-1] else False
+
+
+def options_generator(types_of_leg, types_of_speed, parameter):
+    options = []
+    for leg in types_of_leg:
+        for speed in types_of_speed:
+            options.append(leg + '_' + speed + '_' + parameter)
+    return options
+
+
+def add_additional_consumption(ship):
+
+    text = "\nPlease add additional consumption during {}, mt of MGO\n"
+    ship['additional_consumption'] = {'port_stay': None, 'steaming': None}
+
+    for stage in ['port_stay', 'steaming']:
+        revised_text = text.format(stage)
+        additional_consumption = int(input(revised_text))
+        ship['additional_consumption'][stage] = additional_consumption
+
+    return ship
+
+
+def add_parameter(ship, parameter):
+
+    ship[parameter] = {}
+
+    types_of_leg = ['laden', 'ballast']
+    types_of_speed = ['full', 'eco']
+
+    options = options_generator(types_of_leg, types_of_speed, parameter)
+    for option in options:
+        the_type_of_leg, the_type_of_speed, the_parameter = option.split('_')
+        units = 'kn' if the_parameter == 'speed' else 'mt/day'
+
+        text = "\nPlease add {} {} {} of the ship, {}\n".format(
+            the_type_of_speed, the_type_of_leg, the_parameter, units)
+
+        ship[parameter][option] = int(input(text))
+
+    ship[parameter]["date_of_update"] = "{:%Y-%m-%d}".format(datetime.now())
+    return ship
+
+
+def ship_in_list_checker(name):
+    list_of_ships = read_JSON_file(SHIPS_FILE)
+    if list_of_ships == []:
+        return False
+    for ship in list_of_ships:
+        if ship['ships_name'] == name:
+            print('Ship with this name and IMO is already in list, ' +
+                  'therefore, it will not be added again.')
+            return True
+    return False
+
+
+def voyages_assigned_checker(ship):
+    list_of_dicts = read_JSON_file(LIST_OF_VOYAGES_FILE)
+    ships_with_voyages = list(map(lambda x: x['ship'], list_of_dicts))
+    if ship not in ships_with_voyages:
+        return False
+    return True
---
+++ ships_list/additional_functions/ship/ships_functions.py
@@ -0,0 +1,87 @@
+from ships_list.additional_functions.additional_functions import \
+    list_to_string, list_of_val_by_key, read_dict
+from ships_list.lists.Standard.constants import SHIPS_FILE
+from ships_list.additional_functions.json_functions import append_JSON_file, \
+    read_JSON_file, write_JSON_file
+from ships_list.additional_functions.ship.additional_ship_functions import \
+    add_parameter, voyages_assigned_checker, add_additional_consumption, \
+    IMO_checker, ship_in_list_checker
+
+
+def print_ship():
+    #     root = Tk()
+
+    #     root['bg'] = '#fafafa'
+    #     root.title('Printing ship\'s details')
+    #     root.wm_attributes('-alpha', 0.8)
+    #     root.geometry('400x400')
+
+    #     canvas = Canvas(root)
+    #     canvas.pack()
+
+    #     frame = Frame(root)
+    #     frame.place(relx=0.1, rely=0.1, relwidth=1, relheight=1)
+    #     title = Label(frame, text='Please put name of ship')
+    #     title.pack()
+
+    #     # find_button = Button(frame, text='Find', font=20, bg='yellow')
+
+    #     root.mainloop()
+    return
+
+
+def add_ship():
+
+    name = input('Please enter ship\'s name\n').upper()
+    if not ship_in_list_checker(name):
+        return
+
+    IMO = input('Please enter IMO of the ship\n')
+    if IMO_checker(IMO) is False:
+        return
+
+    ships_details = {"ships_name": name.upper(),
+                     "IMO": int(IMO)}
+
+    ships_details = add_parameter(ships_details, 'speed')
+    ships_details = add_parameter(ships_details, 'consumption')
+    ships_details = add_additional_consumption(ships_details)
+
+    append_JSON_file(ships_details, SHIPS_FILE)
+    print('Ship {} has been added.\n'.format(name.upper()))
+
+
+# removes function from bata base
+def remove_ship():
+
+    list_of_ships = read_JSON_file(SHIPS_FILE)
+    list_of_names = list_to_string(list_of_val_by_key('ships_name',
+                                                      list_of_ships).sort())
+    print('\nList of ships:\n' + list_of_names)
+
+    print('Please put ship\'s name.')
+    ships_name = input().upper()
+
+    if voyages_assigned_checker(ships_name):
+        print('\nShip {} can not be removed due to voyage(s) '.format(
+            ships_name) + 'assigned.')
+        return
+
+    for ship in list_of_ships:
+        if ship['ships_name'] == ships_name:
+            list_of_ships.remove(ship)
+            write_JSON_file(SHIPS_FILE, list_of_ships)
+            print('\nShip ' + ships_name + ' was removed.\n')
+        else:
+            print('\nShip ' + ships_name + ' was not found')
+
+
+def read_ship():
+    ships_name = input('\nPlease add ship\'s name\n').upper()
+    try:
+        the_dict = list(filter(lambda x: x if x['ships_name'] == ships_name
+                               else False, read_JSON_file(SHIPS_FILE)))[0]
+    except IndexError:
+        print('Ship {} is missing in list of ships.'.format(ships_name))
+        return
+    read_dict(the_dict)
