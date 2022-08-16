38fc96e5fd0da566582dc4d1e00f5371e95a5d37
Vitaliy <vitatomchyk@gmail.com>
2022-08-16 11:48:10 +0200

    working on template_manager.py

--- ships_list/lists/Standard/constants.py
+++ ships_list/lists/Standard/constants.py
@@ -1,10 +1,13 @@
 # LINKS
+
+
 SHIPS_FILE = "ships_list/lists/ships.json"
 LIST_OF_VOYAGES_FILE = "ships_list/lists/list_of_voyages.json"
 TASKS_FILE = "ships_list/lists/tasks.json"
 SUPPORTING_FILE = "ships_list/lists/Standard/supporting_info.json"
 BOOKINGS_FILE = "ships_list/lists/bookings.json"
 BUNKERING_FILE = "ships_list/lists/bunkerings.json"
+FILLED_TEMPLATES_FILE = "ships_list/lists/filled_templates.json"
 
 ADDITIONAL_FUNCTIONS_FILE = "ships_list/additional_functions/" + \
     "additional_functions.py"
