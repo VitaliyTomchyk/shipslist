cb03aa96a4b05a0374ec3a8aaf37a69eee028ffb
Dmitriy Chebruchan <chebruchan2707@gmail.com>
2022-08-22 23:04:37 +0300

    input for template was placed inside of function

--- ships_list/additional_functions/templates/template_manager.py
+++ ships_list/additional_functions/templates/template_manager.py
@@ -1,28 +1,26 @@
 # template filler
-from ships_list.lists.Standard.constants import FILLED_TEMPLATES_FILE
-from ships_list.additional_functions.supporting_functions.json_functions \
-    import append_JSON_file
-
-input_data = {
-    "company_name": input("Enter your name of the company: "),
-    "company_to":
-    input("Enter the name of the company you are sending the message to: ")
-}
+# from ships_list.lists.Standard.constants import FILLED_TEMPLATES_FILE
+# from ships_list.additional_functions.supporting_functions.json_functions \
+#     import append_JSON_file
 
 
 def fill_template():
-    input_data
+    input_data = {
+        "company_name": input("Enter your name of the company: "),
+        "company_to":
+        input("Enter the name of the company you are sending the message to: ")
+    }
 
     template = (f'''
 From: {input_data["company_name"]}
 To: {input_data["company_to"]}
 ''')
 
-    template_info = {
-        'input_data': input_data,
-        'text': template
-    }
+    # template_info = {
+    #     'input_data': input_data,
+    #     'text': template
+    # }
 
-    append_JSON_file(template_info, FILLED_TEMPLATES_FILE)
+    # append_JSON_file(template_info, FILLED_TEMPLATES_FILE)
 
     return print(template)
