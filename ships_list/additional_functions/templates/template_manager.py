d56bd9647f8683f55ad118d723e989b8608026da
Dmitriy Chebruchan <chebruchan2707@gmail.com>
2022-08-24 12:24:39 +0300

    tests revised

--- ships_list/additional_functions/templates/template_manager.py
+++ ships_list/additional_functions/templates/template_manager.py
@@ -9,7 +9,6 @@ def fill_template():
 
     tmplt_details = details_generator()
 
-
     data = data_collector(tmplt_details["keys_of_tmpl"],
                           tmplt_details["type_of_template"])
 
