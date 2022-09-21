3e6035dd5a111f47ecdf3cf84a2c7fc39d3ff52d
Dmitriy Chebruchan <chebruchan2707@gmail.com>
2022-09-21 16:31:55 +0300

    refactoring fill_template

--- ships_list/additional_functions/templates/additional_functions.py
+++ ships_list/additional_functions/templates/additional_functions.py
@@ -1,47 +1,44 @@
 # template filler
 from ships_list.additional_functions.supporting_functions.json_functions \
     import read_JSON_file
-from ships_list.additional_functions.supporting_functions.\
-    additional_functions import dict_to_ol_string
-from ships_list.lists.Standard.constants import SUPPORTING_FILE
+from ships_list.lists.Standard.constants import KEYS_OF_TEMPLATES_FILE
+from ships_list.additional_functions.supporting_functions.input_functions \
+    import input_from_list
+import time
 
 
 def template_selector():
+    list_of_templates = read_JSON_file(KEYS_OF_TEMPLATES_FILE)
 
-    list_of_templates = read_JSON_file(SUPPORTING_FILE)['templates']
-    print('\nList of templates:')
-    print(dict_to_ol_string(list_of_templates))
+    names_of_templates = list(map(lambda x: x['template_name'],
+                                  list_of_templates))
 
-    template_name = str(input('\nPlease enter name of template:\n'))
+    name_of_template = input_from_list(
+        'name of template', names_of_templates)
 
-    result = {
-        "template_address": list_of_templates[f'{template_name}']
-        ['address_of_template'],
-        "keys_of_tmplt": list_of_templates[f'{template_name}']
-        ['keys_of_tmplt'],
-        "type_of_template": template_name}
+    template = list(filter(lambda x: x['template_name'] == name_of_template,
+                           list_of_templates))[0]
 
-    return result
-
-
-def details_generator():
-
-    the_template = template_selector()
-
-    return the_template["template_address"],\
-        the_template["type_of_template"], the_template["keys_of_tmplt"]
-
-
-def data_collector(keys_of_tmplt, type_of_template):
-    result = {key: input('\nPlease input {} \n'.format(key))
-              for key in keys_of_tmplt}
-    result['type_of_template'] = type_of_template
-    return result
+    return template
 
 
 def creating_address_for_filled_template():
-    address = 'ships_list/files/filled_templates/{}.txt'.format(
-        input('\nPlease enter name of filled template\n').upper())
-
-    print('\nAddress of filled template: {}\n'.format(address))
+    file_name = input('\nPlease enter name of filled template\n')\
+        .upper()\
+        .replace(" ", "_")
+    address = 'ships_list/files/filled_templates/{}.txt'.format(file_name)
+    time.sleep(2)
+    print('\nAddress of filled template: \n{}\n'.format(address))
     return address
+
+
+def values_from_user(template):
+    keys = template['keys_of_tmplt']
+
+    result = {}
+    for key in keys:
+        instruction = '\nPlease put value of "{}" to fill template\n'\
+            .format(key)
+        result[key] = input(instruction)
+
+    return result
--- ships_list/additional_functions/templates/template_manager.py
+++ ships_list/additional_functions/templates/template_manager.py
@@ -2,32 +2,42 @@ from string import Template
 from ships_list.additional_functions.supporting_functions.json_functions \
     import append_JSON_file
 from ships_list.additional_functions.templates.additional_functions import \
-    details_generator, data_collector, creating_address_for_filled_template
+    template_selector, creating_address_for_filled_template, values_from_user
 from ships_list.additional_functions.supporting_functions.\
     additional_functions import assurance_question
+from ships_list.lists.Standard.constants import FILLED_TEMPLATES_FILE
+import time
 
 
 def fill_template():
 
+    # assurance
     assurance_question('fill a template')
 
-    tmplt_details = details_generator()
+    # selecting template
+    selected_template = template_selector()
 
-    data = data_collector(tmplt_details[2],
-                          tmplt_details[1])
+    # collecting values to put in template from user
+    dict_with_input_keys_and_values = values_from_user(selected_template)
 
-    with open(tmplt_details[0], 'r') as template_file:
+    # collecting template information
+    with open(selected_template['address_of_template'], 'r') as template_file:
         temp_obj = Template(template_file.read())
-    str_with_result = temp_obj.substitute(**data)
 
+    # substituting vars with values
+    str_with_result = temp_obj.substitute(**dict_with_input_keys_and_values)
+
+    # saving filled template
     filled_template_address = creating_address_for_filled_template()
     with open(filled_template_address, 'w') as f:
         f.write(str_with_result)
 
-    data = {'filled_template_address': filled_template_address}
+    # saving address of filled template
+    data = dict_with_input_keys_and_values
+    data['filled_template_address'] = filled_template_address
+    append_JSON_file(data, FILLED_TEMPLATES_FILE)
 
-    address_of_json_file = 'ships_list/lists/data_of_templates.json'
-    append_JSON_file(data, address_of_json_file)
-
-    print('Template filled and saved.')
-    print('Filled template is \n\n{}'.format(str_with_result))
+    # reporting to user
+    time.sleep(2)
+    print('Template has been filled and saved.\n' +
+          'Filled template is \nQuote\n{}\nUnquote'.format(str_with_result))
