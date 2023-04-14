dc32db4ea5999173100e6f007cccc21a4b0f204e
Dmitriy Chebruchan <chebruchan2707@gmail.com>
2023-04-14 17:09:08 +0300

    refactoring to use classes for entities person and company

--- ships_list/additional_functions/classes.py
+++ ships_list/additional_functions/classes.py
@@ -1,5 +1,11 @@
 import json
 from ships_list.lists.Standard.constants import SUPPORTING_FILE
+from ships_list.additional_functions.supporting_functions.json_functions \
+    import append_JSON_file, read_JSON_file, write_JSON_file
+from ships_list.additional_functions.supporting_functions.input_functions \
+    import input_from_list
+from ships_list.additional_functions.supporting_functions.additional_functions\
+    import id_generator
 
 
 class Task():
@@ -53,3 +59,82 @@ class Ships_list():
         print('Ships list name is \n{}'.format(self.name_of_list))
         print('Ships name is {}'.format(self.name_of_ship))
         print('Ships list \n{}'.format(self.task_list))
+
+# function input_email
+
+
+def input_email(element):
+    # input email
+    email = input('Please enter email of {}\n'.format(element))
+    # checking if email is valid
+    if not email or '@' not in email:
+        print('Email is not valid. Please try again.')
+        email = input_email()
+    # returning email
+    return email
+
+
+class StoredElement:
+    # class constructor
+    def __init__(self, entity, file):
+        self.entity = entity
+        self.file = file
+
+    # function to create an element
+    def create_element(self):
+        element = {
+            'id': id_generator(),
+            'name': input(f'Please enter {self.entity} name\n'),
+            'email': input_email(
+                self.entity),
+            'phone_number': input('Please enter' +
+                                  f' phone number of {self.entity}\n'),
+            'address': input(f'Please enter address of {self.entity}\n'),
+        }
+        append_JSON_file(element, self.file)
+        print(
+            f'\nCreated {self.entity} with the following details:\n{element}')
+
+    # function to remove an element
+    def remove_element(self):
+        list_of_elements = read_JSON_file(self.file)
+        if not list_of_elements:
+            print('There are no elements to remove.')
+            return
+
+        list_of_names = [element['name'] for element in list_of_elements]
+        element_to_remove = input_from_list(
+            f'{self.entity} to remove', list_of_names)
+        result = [
+            element for element in list_of_elements
+            if element['name'] != element_to_remove]
+
+        write_JSON_file(self.file, result)
+        print(f'{self.entity} {element_to_remove} was removed')
+
+    # function to edit an element
+    def edit_element(self):
+        list_of_elements = read_JSON_file(self.file)
+        if not list_of_elements:
+            print('There are no elements to edit.')
+            return
+
+        list_of_names = [element['name'] for element in list_of_elements]
+        element_name_to_edit = input_from_list(
+            f'{self.entity} to edit', list_of_names)
+
+        element_to_edit = next(
+            (element for element in list_of_elements
+             if element['name'] == element_name_to_edit),
+            None)
+        if not element_to_edit:
+            print(f'{self.entity} {element_name_to_edit} not found')
+            return
+
+        field_to_edit = input_from_list(
+            'field to edit', [
+                'name', 'email', 'phone_number', 'address'])
+        new_value = input(f'Please enter new value of field {field_to_edit}\n')
+        element_to_edit[field_to_edit] = new_value
+
+        write_JSON_file(self.file, list_of_elements)
--- ships_list/ships_list/ships_list.py
+++ ships_list/ships_list/ships_list.py
@@ -12,14 +12,12 @@ from ships_list.additional_functions.templates.template_manager \
     import fill_template, remove_template
 from ships_list.additional_functions.templates.create_template.create_template\
     import create_template
-from ships_list.additional_functions.companies.companies_module import \
-    add_company, remove_company, edit_company
-from ships_list.additional_functions.people.people_module import add_person,\
-    remove_person, edit_person
 from ships_list.additional_functions.positions.positions import \
     add_position, check_position_status
 from ships_list.additional_functions.indexes.indexes import \
     add_index
+from ships_list.additional_functions.classes import StoredElement
+from ships_list.lists.Standard.constants import PEOPLE_FILE, COMPANIES_FILE
 
 
 def ships_list(parced_result):
@@ -29,40 +27,41 @@ def ships_list(parced_result):
         'ship_to_remove': remove_ship,
         'remove_ship': remove_ship,
         'print_ship': print_ship,
-
         'add_task': add_task,
         'amend_task': amend_task,
         'remove_task': remove_task,
         "read_tasks_list": read_tasks_list,
         "close_task": close_task,
-
         'add_voyage': add_voyage,
         'remove_voyage': remove_voyage,
-
         'add_booking': add_booking,
         'read_booking': read_booking,
         'remove_booking': remove_booking,
-
         'freight_calculator': freight_calculator,
-
         'fill_template': fill_template,
         'create_template': create_template,
         'remove_template': remove_template,
-
-        'add_company': add_company,
-        'remove_company': remove_company,
-        'edit_company': edit_company,
-
-        'add_person': add_person,
-        'remove_person': remove_person,
-        'edit_person': edit_person,
-
+        'add_company': StoredElement(
+            'company',
+            COMPANIES_FILE).create_element,
+        'remove_company': StoredElement(
+            'company',
+            COMPANIES_FILE).remove_element,
+        'edit_company': StoredElement(
+            'company',
+            COMPANIES_FILE).edit_element,
+        'add_person': StoredElement(
+            'person',
+            PEOPLE_FILE).create_element,
+        'remove_person': StoredElement(
+            'person',
+            PEOPLE_FILE).remove_element,
+        'edit_person': StoredElement(
+            'person',
+            PEOPLE_FILE).edit_element,
         'add_position': add_position,
         'check_position_status': check_position_status,
-
-        'add_index': add_index
-
-    }
+        'add_index': add_index}
 
     function = list(filter(lambda x: parced_result[x], parced_result))[0]
     options[function]()
