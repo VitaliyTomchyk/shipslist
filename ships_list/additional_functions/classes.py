337747b88496f766b3978debc84181ce0e9d1f3d
Dmitriy Chebruchan <chebruchan2707@gmail.com>
2023-04-17 12:24:16 +0300

    refactoring of usage of classes

--- ships_list/additional_functions/classes.py
+++ ships_list/additional_functions/classes.py
@@ -1,66 +1,9 @@
-import json
-from ships_list.lists.Standard.constants import SUPPORTING_FILE
 from ships_list.additional_functions.supporting_functions.json_functions \
     import append_JSON_file, read_JSON_file, write_JSON_file
 from ships_list.additional_functions.supporting_functions.input_functions \
     import input_from_list
 from ships_list.additional_functions.supporting_functions.additional_functions\
-    import id_generator
-
-
-class Task():
-    def __init__(self, title, stage, ship):
-        self.ID = 1
-        self.title = title
-        self.stage = stage
-        self.ship = ship
-        self.status = 'Pending'
-
-    def close_task(self):
-        self.status = 'Done'
-
-    def read_task(self):
-        print('Task is \n{}\nShip is \n{}\n'.format(self.title, self.ship) +
-              'Stage is \n{}\nStatus is {}').format(self.stage, self.status)
-
-
-class Ship():
-    def __init__(self, name, IMO):
-        self.name = name
-        self.IMO = IMO
-        self.has_list = False
-        self.list_related = None
-
-    def set_list(self, name, lists_id):
-        self.has_list = True
-        self.list_related = lists_id
-
-    def read_ship(self):
-        print("Ship is {}\n IMO is {}\n Has list? {}".format(self.name,
-                                                             self.IMO,
-                                                             self.has_list))
-
-
-class Ships_list():
-    def __init__(self, name_of_ship, title_of_voyage):
-        self.ships_list_id = 1
-        self.name_of_list = title_of_voyage
-        self.name_of_ship = name_of_ship
-
-        with open(SUPPORTING_FILE, 'r') as f:
-            standard_list = json.load(f)
-
-        for task in standard_list:
-            task["ship"] = name_of_ship
-
-        self.task_list = standard_list
-
-    def read_ships_list(self):
-        print('Ships list name is \n{}'.format(self.name_of_list))
-        print('Ships name is {}'.format(self.name_of_ship))
-        print('Ships list \n{}'.format(self.task_list))
-
-# function input_email
+    import id_generator, read_dict
 
 
 def input_email(element):
@@ -88,14 +31,31 @@ class StoredElement:
             'email': input_email(
                 self.entity),
             'phone_number': input('Please enter' +
-                                  f' phone number of {self.entity}\n'),
-            'address': input(f'Please enter address of {self.entity}\n'),
+                                  f' phone number of {self.entity}\n')
         }
+
+        # appending element with keys 'company' and 'position' and values from
+        # user if self.entity is 'person'
+        if self.entity == 'person':
+            element['company'] = input('Please enter company name\n')
+            element['position'] = input('Please enter position\n')
+
+        # appending element with keys 'address' and 'web site' and values from
+        # user if self.entity is 'company'
+        if self.entity == 'company':
+            element['address'] = input('Please enter company address\n')
+            element['web_site'] = input('Please enter web site\n')
+
         append_JSON_file(element, self.file)
+
+        # printing description of created element
+        description_of_element = read_dict(element)
         print(
-            f'\nCreated {self.entity} with the following details:\n{element}')
+            f'\nCreated {self.entity} with the following details:\n' +
+            f'{description_of_element}\n')
 
     # function to remove an element
+
     def remove_element(self):
         list_of_elements = read_JSON_file(self.file)
         if not list_of_elements:
@@ -138,3 +98,9 @@ class StoredElement:
         element_to_edit[field_to_edit] = new_value
 
         write_JSON_file(self.file, list_of_elements)
+
+        # printing description of edeted element
+        description_of_element = read_dict(element_to_edit)
+        print(
+            f'\nCreated {self.entity} with the following details:\n' +
+            f'{description_of_element}\n')
